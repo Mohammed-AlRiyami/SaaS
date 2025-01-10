14fb26004de848824d8ba0cedd0f0c4dd20cbb22
Mohammed-AlRiyami <mohammed.alriyami2003@gmail.com>
2025-01-09 23:30:50 -0600

    Added visits app

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -1,14 +1,23 @@
 from django.http import HttpResponse
 import pathlib
 from django.shortcuts import render
+from visits.models import PageVisit
+
 
 def home_home_view(request, *args, **kwargs): 
+    # queryset = PageVisit.objects.all()
+    qs = PageVisit.objects.all()
+    page_qs = PageVisit.objects.filter(path=request.path)
     my_title = "Mo's Page"
     my_context = {
         "page_title": my_title,
+        "page_visit_count": page_qs.count(), 
+        "percent": (page_qs.count()/qs.count()*100),
+        "total_visit_count": qs.count()
     }
-    html_template = "home.html"
     
+    html_template = "home.html"
+    PageVisit.objects.create(path=request.path,)
     return render(request, html_template, my_context)
 
 # def home_home_view(request, *args, **kwargs): 
