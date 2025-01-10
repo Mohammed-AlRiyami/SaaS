98534cbb1fa362b81263b614faaac3928e4f75b5
Mohammed-AlRiyami <mohammed.alriyami2003@gmail.com>
2025-01-09 23:53:19 -0600

    Updated views.py

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -9,6 +9,8 @@ def home_home_view(request, *args, **kwargs):
     qs = PageVisit.objects.all()
     page_qs = PageVisit.objects.filter(path=request.path)
     my_title = "Mo's Page"
+    html_template = "home.html"
+
     my_context = {
         "page_title": my_title,
         "page_visit_count": page_qs.count(), 
@@ -16,7 +18,6 @@ def home_home_view(request, *args, **kwargs):
         "total_visit_count": qs.count()
     }
     
-    html_template = "home.html"
     PageVisit.objects.create(path=request.path,)
     return render(request, html_template, my_context)
 
