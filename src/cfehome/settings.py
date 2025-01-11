02f02efa68a5e75463c29ae0d81dcc13ac6d65c0
Mohammed-AlRiyami <mohammed.alriyami2003@gmail.com>
2025-01-10 23:54:58 -0600

    just a push

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("DJANGO_SECRET_KEY")
+SECRET_KEY = 'django-insecure-)y1i9(a1(c)qq7p(7(8f)l%4-x(y37ov3my^l(o16@w-bxesx1'
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower()=="true"
 DEBUG = config("DJANGO_DEBUG", cast=bool)
