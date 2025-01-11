7adf300a23a7a45eea8bffd8d92382a8d79fd3f4
Mohammed-AlRiyami <mohammed.alriyami2003@gmail.com>
2025-01-11 00:03:57 -0600

    fixing errors

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-import os
 from pathlib import Path
 from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)y1i9(a1(c)qq7p(7(8f)l%4-x(y37ov3my^l(o16@w-bxesx1'
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower()=="true"
 DEBUG = config("DJANGO_DEBUG", cast=bool)
